22acfb9acca9165c832c796344b139bd4418c2fc
Joshua Robinson <joshua@codeadventure.uk>
2024-09-09 16:08:00 +0100

    installed @types/express

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require('express');
+const app = express();
+// apiRouter = require('./routes/api-router.ts');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+//app.use('/api', apiRouter) // Route Handling;
+//Write Error Handling Below This Line
+module.exports = app;
