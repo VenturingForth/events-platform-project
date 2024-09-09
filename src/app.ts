333f596e0b566ba11ede499c06340d90a9e65b20
Joshua Robinson <joshua@codeadventure.uk>
2024-09-09 12:25:33 +0100

    added structure to the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+// apiRouter = require('./routes/api-router.ts');
 const cors = require('cors');
 
 app.use(cors());
 
 app.use(express.json());
+
+//app.use('/api', apiRouter) // Route Handling;
+
+//Write Error Handling Below This Line
 
 module.exports(app);
