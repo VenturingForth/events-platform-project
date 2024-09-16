3348c1b341847efd15794fa02e5b82e48f49490f
Joshua Robinson <joshua@codeadventure.uk>
2024-09-16 13:46:40 +0100

    Added run-seed file for dev data

---
+++ src/db/seeds/run-seed.ts
@@ -0,0 +1,13 @@
+import { devData } from "../data/development-data/index";
+
+const runSeed = () => {
+
+    const { seed } = require("./seed");
+    const { db } = require("../connection");
+
+    seed(devData).then(() => {
+        return db.end();
+    });
+}
+
+runSeed();
